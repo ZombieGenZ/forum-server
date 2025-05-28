77c372ad8b2941cbff33a278d9d1c5a2e52dcd43
ZombieGenZ <zombiegenzzz@gmail.com>
2025-05-28 11:00:44 +0700

    feat: Add colorTextValidator middleware for text color validation in topic creation

--- src/middlewares/colors.middlewares.ts
+++ src/middlewares/colors.middlewares.ts
@@ -18,7 +18,7 @@ export const colorValidator = (req: Request, res: Response, next: NextFunction) 
 
   if (
     body.colorType === ColorType.COLOR_BASIC &&
-    (!body.BasicColor || typeof body.BasicColor !== 'string' || isValidHexColor(body.BasicColor))
+    (!body.basicColor || typeof body.basicColor !== 'string' || isValidHexColor(body.basicColor))
   ) {
     res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
       code: RESPONSE_CODE.COLOR_INVALID,
@@ -33,7 +33,7 @@ export const colorValidator = (req: Request, res: Response, next: NextFunction) 
       !body.gradient2Color2 ||
       typeof body.gradient2Color1 !== 'string' ||
       typeof body.gradient2Color2 !== 'string' ||
-      !isValidHexColor(body.radient2Color1) ||
+      !isValidHexColor(body.gradient2Color1) ||
       !isValidHexColor(body.gradient2Color2))
   ) {
     res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
@@ -54,6 +54,66 @@ export const colorValidator = (req: Request, res: Response, next: NextFunction) 
       !isValidHexColor(body.gradient3Color1) ||
       !isValidHexColor(body.gradient3Color2) ||
       !isValidHexColor(body.gradient3Color3))
+  ) {
+    res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
+      code: RESPONSE_CODE.COLOR_INVALID,
+      message: MESSAGE.COLOR_MESSAGE.GRADIENT_3_COLOR_IS_REQUIRED
+    })
+    return
+  }
+
+  next()
+}
+
+export const colorTextValidator = (req: Request, res: Response, next: NextFunction) => {
+  const body = req.body
+
+  if (!body.textColorType) {
+    res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
+      code: RESPONSE_CODE.COLOR_INVALID,
+      message: MESSAGE.COLOR_MESSAGE.COLOR_TYPE_IS_REQUIRED
+    })
+    return
+  }
+
+  if (
+    body.textColorType === ColorType.COLOR_BASIC &&
+    (!body.textBasicColor || typeof body.textBasicColor !== 'string' || isValidHexColor(body.textBasicColor))
+  ) {
+    res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
+      code: RESPONSE_CODE.COLOR_INVALID,
+      message: MESSAGE.COLOR_MESSAGE.BASIC_COLOR_IS_REQUIRED
+    })
+    return
+  }
+
+  if (
+    body.textColorType === ColorType.COLOR_GRADIENT_2 &&
+    (!body.textGradient2Color1 ||
+      !body.textGradient2Color2 ||
+      typeof body.textGradient2Color1 !== 'string' ||
+      typeof body.textGradient2Color2 !== 'string' ||
+      !isValidHexColor(body.textGradient2Color1) ||
+      !isValidHexColor(body.textGradient2Color2))
+  ) {
+    res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
+      code: RESPONSE_CODE.COLOR_INVALID,
+      message: MESSAGE.COLOR_MESSAGE.GRADIENT_2_COLOR_IS_REQUIRED
+    })
+    return
+  }
+
+  if (
+    body.textColorType === ColorType.COLOR_GRADIENT_3 &&
+    (!body.textGradient3Color1 ||
+      !body.textGradient3Color2 ||
+      !body.textGradient3Color3 ||
+      typeof body.textGradient3Color1 !== 'string' ||
+      typeof body.textGradient3Color2 !== 'string' ||
+      typeof body.textGradient3Color3 !== 'string' ||
+      !isValidHexColor(body.textGradient3Color1) ||
+      !isValidHexColor(body.textGradient3Color2) ||
+      !isValidHexColor(body.textGradient3Color3))
   ) {
     res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json({
       code: RESPONSE_CODE.COLOR_INVALID,
--- src/routes/topics.routes.ts
+++ src/routes/topics.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { createTopicController } from '~/controllers/topics.controllers'
 import { authenticateAdministratorValidator, authenticateValidator } from '~/middlewares/authenticate.middlewares'
-import { colorValidator } from '~/middlewares/colors.middlewares'
+import { colorTextValidator, colorValidator } from '~/middlewares/colors.middlewares'
 import { createTopicValidator } from '~/middlewares/topics.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers.utils'
 const router = express.Router()
@@ -17,12 +17,19 @@ const router = express.Router()
  *    refresh_token: string,
  *    topic: string,
  *    colorType: string,
- *    BasicColor?: string,
+ *    basicColor?: string,
  *    gradient2Color1?: string,
  *    gradient2Color2?: string,
  *    gradient3Color1?: string,
  *    gradient3Color2?: string,
- *    gradient3Color3?: string
+ *    gradient3Color3?: string,
+ *    textColorType: string,
+ *    textBasicColor?: string,
+ *    textGradient2Color1?: string,
+ *    textGradient2Color2?: string,
+ *    textGradient3Color1?: string,
+ *    textGradient3Color2?: string,
+ *    textGradient3Color3?: string
  * }
  */
 router.post(
@@ -31,6 +38,7 @@ router.post(
   authenticateAdministratorValidator,
   createTopicValidator,
   colorValidator,
+  colorTextValidator,
   wrapRequestHandler(createTopicController)
 )
 
